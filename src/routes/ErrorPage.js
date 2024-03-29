3f622ba8355aa2a072fd09846cbf688651cc32e3
okhergo <okhergo@usal.com>
2024-03-30 00:34:44 +0100

    new error page

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -5,7 +5,7 @@ const { Content } = Layout
 
 const ErrorPage = () => {
   return (
-    <Content className='Content'>
+    <Content className='Error'>
         <h1>ERROR 404:</h1>
         <h2>Page not found</h2>
     </Content>
